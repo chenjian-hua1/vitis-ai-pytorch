61ad563bd7920ccbe75f7d79953ab8a2313de0a8
chenjian-hua <yourgitemail@example.com>
2026-04-14 15:19:16 +0800

    upload build sh

---
+++ dpu_inference/cpp/sim_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SRC_FILE="$1"
+OUT_DIR="$(dirname "$SRC_FILE")/build"
+OUT_FILE="$OUT_DIR/$(basename "${SRC_FILE%.*}")"
+
+mkdir -p "$OUT_DIR"
+
+# 透過 pkg-config 取得 flags
+CXXFLAGS=$(pkg-config --cflags opencv4)
+LIBS=$(pkg-config --libs opencv4)
+
+/usr/bin/clang++ \
+    -fcolor-diagnostics \
+    -fansi-escape-codes \
+    -g \
+    "$SRC_FILE" \
+    $CXXFLAGS \
+    $LIBS \
+    -o "$OUT_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "✅ Build success: $OUT_FILE"
+else
+    echo "❌ Build failed"
+fi
